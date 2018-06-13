3794d77b14389f0911cd79042d5d2aaef36e6e76
mka <mka281@icloud.com>
2018-06-13 16:41:32 +0300

    Connect to database from application.py

--- application.py
+++ application.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Category, CategoryItem
+
+
 app = Flask(__name__)
+
+# Connect to DB
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.bind = engine
+
+# Create DB session
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/')
 def HelloWorld():
     return "Hello World"
+
 
 if __name__ == '__main__':
     app.debug = True
