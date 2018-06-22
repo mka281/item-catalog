5592e1636833cd4244d6f0f076ba808919f3c0cc
mka <mka281@icloud.com>
2018-06-22 19:01:15 +0300

    Add file to generate sample items

---
+++ fakeItems.py
@@ -0,0 +1,159 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Category, CategoryItem
+from sqlalchemy.sql import func
+
+
+engine = create_engine('sqlite:///itemcatalog.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+
+# Items for Category 1
+category1 = Category(name="Category 1", user_id=1)
+
+session.add(category1)
+session.commit()
+
+category_item1 = CategoryItem(name="Item 1",
+                              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                              created_date=func.current_timestamp(),
+                              category_id=1,
+                              user_id=1)
+
+session.add(category_item1)
+session.commit()
+
+category_item2 = CategoryItem(name="Item 2",
+                              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                              created_date=func.current_timestamp(),
+                              category_id=1,
+                              user_id=1)
+
+session.add(category_item2)
+session.commit()
+
+category_item3 = CategoryItem(name="Item 3",
+                              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                              created_date=func.current_timestamp(),
+                              category_id=1,
+                              user_id=1)
+
+session.add(category_item3)
+session.commit()
+
+category_item4 = CategoryItem(name="Item 4",
+                              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                              created_date=func.current_timestamp(),
+                              category_id=1,
+                              user_id=1)
+
+session.add(category_item4)
+session.commit()
+
+
+# Items for Category 2
+category2 = Category(name="Category 2", user_id=1)
+
+session.add(category2)
+session.commit()
+
+category_item1 = CategoryItem(name="Item 5",
+                              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                              category_id=2,
+                              created_date=func.current_timestamp(),
+                              user_id=2)
+
+session.add(category_item1)
+session.commit()
+
+category_item2 = CategoryItem(name="Item 6",
+                              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                              created_date=func.current_timestamp(),
+                              category_id=2,
+                              user_id=2)
+
+session.add(category_item2)
+session.commit()
+
+category_item3 = CategoryItem(name="Item 7",
+                              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                              created_date=func.current_timestamp(),
+                              category_id=2,
+                              user_id=2)
+
+session.add(category_item3)
+session.commit()
+
+category_item4 = CategoryItem(name="Item 8",
+                              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                              created_date=func.current_timestamp(),
+                              category_id=2,
+                              user_id=2)
+
+session.add(category_item4)
+session.commit()
+
+
+# Items for Category 3
+category3 = Category(name="Category 3", user_id=2)
+
+session.add(category3)
+session.commit()
+
+category_item1 = CategoryItem(name="Item 9",
+                              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                              category_id=3,
+                              created_date=func.current_timestamp(),
+                              user_id=3)
+
+session.add(category_item1)
+session.commit()
+
+category_item2 = CategoryItem(name="Item 10",
+                              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                              created_date=func.current_timestamp(),
+                              category_id=3,
+                              user_id=3)
+
+session.add(category_item2)
+session.commit()
+
+category_item3 = CategoryItem(name="Item 11",
+                              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                              created_date=func.current_timestamp(),
+                              category_id=3,
+                              user_id=3)
+
+session.add(category_item3)
+session.commit()
+
+category_item4 = CategoryItem(name="Item 12",
+                              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                              created_date=func.current_timestamp(),
+                              category_id=3,
+                              user_id=3)
+
+session.add(category_item4)
+session.commit()
+
+
+# Set total_items to 4 for all categories
+category1 = session.query(Category).filter_by(id=1).one()
+category2 = session.query(Category).filter_by(id=2).one()
+category3 = session.query(Category).filter_by(id=3).one()
+category1.total_item = category2.total_item = category3.total_item = 4
+session.commit()
+
+print("Added category items!")
